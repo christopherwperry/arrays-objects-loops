e2f6c3182323a82b21388f978755d5c6d70009dc
christopherwperry <christopherwperry@gmail.com>
2017-07-27 13:51:20 -0400

    completed all questions with no errors on test.js page

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let numbers = [ 1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6 ];
 1. Return the 4th element in the "numbers" array
 */
 function getFourthNum(){
-    // Your answer here:
+  return numbers[3];
 }
 
 /*
@@ -23,14 +23,20 @@ function getFourthNum(){
 */
 function smallNums(){
     let smallNums = [];
-    // Your answer here:
+    for (let i = 0; i < numbers.length; i++){
+      if (numbers[i] < 10){
+          smallNums.push(numbers[i]);
+      }
+    }
+    return smallNums;
 }
 
 /*
 3. Add 12, 99, and 101 (in that order) to the end of the "numbers" array. Return the "numbers" array.
 */
 function addNums(){
-    // Your answer here:
+  numbers.push(12,99,101);
+  return numbers;
 }
 
 
@@ -53,26 +59,24 @@ let film = {
 4. Add a property "boxoffice" with a value of 269061 to the "film" object. Return "film".
 */
 function addBoxOffice(){
-    // Your answer here:
+  film.boxoffice = 269061;
+  return film;
 }
 
 /*
 5. Add the name "Yukiko Shimazaki" to the "actors" array. Return "film";
 */
 function addActor(){
-    // Your answer here:
+  film.actors.push("Yukiko Shimazaki");
+  return film;
 }
 
 /*
 6. Now that you've added a "boxoffice" property, subtract "budget" from "property" and return the difference. This number is the amount lost by the studio in making the film. Return the loss.
 */
 function getLosses(){
-    // Your answer here:
+  return film.boxoffice - film.budget;
 }
-
-
-
-
 
 
 /*
@@ -86,12 +90,11 @@ let letterVals = [ "v", "x", "r", "f", "p" ];
 
 function interleave(){
     let vals = [];
-    // Your answer here:
+    for( let i = 0; i < numberVals.length; i++){
+      vals.push(letterVals[i] + numberVals[i]);
+    }
+    return vals;
 }
-
-
-
-
 
 
 /*
@@ -103,5 +106,10 @@ let second = [ "think", "stand", "cheese", "break", "numinous", "mouse", "close"
 
 function union(){
     let same = [];
-    // Your answer here:
+    for( let i = 0; i < first.length; i++){
+      if(first[i] === second[i]){
+        same.push(first[i]);
+      }
+    }
+    return same;
 }
